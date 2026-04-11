4d9f3fcb131fedb8578a139cca1b49c7940080f1
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-04-11 01:20:51 -0600

    trigger deploy

--- webpack.config.ts
+++ webpack.config.ts
@@ -9,7 +9,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const APP_NAME = 'mfe_app_one';
+const APP_NAME = 'pe_mfe_budget';
 
 const config = {
   ...createWebpackConfig({
