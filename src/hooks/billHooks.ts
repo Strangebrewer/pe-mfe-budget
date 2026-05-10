c09a1f02768d5a7f361dc19ce1b16be679bdbd31
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-05-10 05:32:17 -0600

    update prettier

--- src/hooks/billHooks.ts
+++ src/hooks/billHooks.ts
@@ -9,6 +9,7 @@ export const useGetBills = (month: string) => {
       const { data = [] } = await billApi.get({ month });
       return data;
     },
+    staleTime: 5 * 60 * 1000, // 5 minutes
   });
 };
 
@@ -37,9 +38,18 @@ export const usePayBill = () => {
       const response = await billApi.payBill(billId, rest);
       return response.data;
     },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['get-bills'] });
-      queryClient.invalidateQueries({ queryKey: ['get-transactions'] });
+    onSuccess: (newTxn, vars) => {
+      queryClient.setQueryData(
+        ['get-bills', vars.month],
+        (old: any[] | undefined) => {
+          if (!old) return old;
+          return old.map((bill) => {
+            return bill.id === vars.billId
+              ? { ...bill, transactions: [...bill.transactions, newTxn] }
+              : bill;
+          });
+        },
+      );
       markTransferStale();
     },
   });
